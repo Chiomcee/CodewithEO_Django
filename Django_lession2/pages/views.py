359dff6d3dd2100e3425c8a2717e0d616d85af6a
Chioma Igwe <cigwe072013@gmail.com>
2024-02-11 07:54:48 +0100

    adding templates

--- Django_lession2/pages/views.py
+++ Django_lession2/pages/views.py
@@ -6,17 +6,21 @@ from django.http import HttpResponse
 
 # homepage
 def home(request):
-    return HttpResponse("<h1>Home  page")
+    # return HttpResponse("<h1>Home  Page")
+    return render(request, 'pages/home.html')
 
 # about us
 def about(request):
-    return HttpResponse("<h1>About us  page")
+    # return HttpResponse("<h1>About us  Page")
+    return render(request, 'pages/about.html')
 
 # contact us
 def contact(request):
-    return HttpResponse("<h1>Contact us  page")
+    # return HttpResponse("<h1>Contact us  Page")
+    return render(request, 'pages/contact.html')
 
 # services
 def services(request):
-    return HttpResponse("<h1>Services  page")
+    # return HttpResponse("<h1>Services  Page")
+    return render(request, 'pages/services.html')
 
